d83d854699027284da312235d3d7cd0d2f2adea0
IGUENIlyes <l_igueni@estin.dz>
2026-03-02 14:13:11 +0100

    finalisations des classes Mammifere et Oiseau : par Jonas-Mukuri-Maka

---
+++ src/main/java/com/zoo/gestion/model/Mammifere.java
@@ -0,0 +1,40 @@
+package main.java.com.zoo.gestion.model;
+
+public class Mammifere extends Animal {
+    private boolean produitLait;
+    private boolean isCarnivore;
+
+    public Mammifere(String name, int age, double poids, String species, String type, String habitat, boolean produitLait, boolean isCarnivore) {
+        super(name, age, poids, species, "Mammifere", habitat);
+        this.produitLait = produitLait;
+        this.isCarnivore = isCarnivore;
+    }
+
+    @Override
+    public double getDailyFood() {
+
+    double base = 0.03 * poids; // 3% du poids corporel
+
+    double carnivoreBonus = isCarnivore ? 1.5 : 0.0;
+
+    double laitBonus = produitLait ? 0.5 : 0.0;
+
+    return base + carnivoreBonus + laitBonus;
+}
+
+    public boolean isProduitLait() {
+        return produitLait;
+    }
+
+    public void setProduitLait(boolean produitLait) {
+        this.produitLait = produitLait;
+    }
+
+    public boolean isCarnivore() {
+        return isCarnivore;
+    }
+
+    public void setCarnivore(boolean carnivore) {
+        isCarnivore = carnivore;
+    }
+}
---
+++ src/main/java/com/zoo/gestion/model/Oiseau.java
@@ -0,0 +1,62 @@
+package main.java.com.zoo.gestion.model;
+
+public class Oiseau extends Animal{
+    private double envergure;
+    private boolean vole;
+    private String beaktype;
+    
+    public Oiseau(String name, int age, String species, String Type, String habitat, double envergure, boolean vole, String beaktype){
+        super(name, age, species,"Oiseau", habitat);
+
+        if (envergure <=0) {
+            throw new IllegalArgumentException("L'envergure doit être positive");
+
+        }
+        this.envergure = envergure;
+        this.vole = vole;
+        if (beaktype == null || beaktype.isEmpty()) {
+            throw new IllegalArgumentException("Le type de bec ne peut pas être vide.");
+        }
+        this.beaktype = beaktype;
+
+    }
+
+    // Getters
+    public double getenvergure() { return envergure; }
+    public boolean isvole() { return vole; }
+    public String getbeaktype() {
+        return beaktype;
+    }
+
+    // Setters
+    public void setenvergure(double envergure) {
+        if (envergure <= 0) {
+            throw new IllegalArgumentException("L'envergure doit être positive");
+        }
+        this.envergure = envergure;
+    }
+    public void setVole(boolean vole) {
+        this.vole = vole;
+    }
+    public void setBeaktype(String beaktype) {
+        if (beaktype == null || beaktype.isEmpty()) {
+            throw new IllegalArgumentException("Le type de bec ne peut pas être vide.");
+        }
+        this.beaktype = beaktype;
+    }
+    
+// Méthodes spécifiques aux oiseaux
+    @Override
+    public double getDailyFood() {
+        return 0.2 * envergure; // Calcul de la nourriture quotidienne basé sur l'envergure
+    }
+
+    @Override
+    public String move(){
+        if(vole){
+            return "je vole dans le ciel";
+        }else{
+            return "Je marche sur le sol";
+        }
+    }
+}
